0e18a0e3d8c2114e8053b1cc13d98565a47229f4
Yingming Luo <Yingming.luo@gmail.com>
2021-12-04 23:41:26 +0000

    feat: added api function to delete and update an existing contact's details

--- phone_book/contacts/api_views.py
+++ phone_book/contacts/api_views.py
@@ -1,5 +1,5 @@
-from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView, CreateAPIView
+from django.db.models.query import QuerySet
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, RetrieveUpdateAPIView
 
 from contacts.serializers import PersonSerializer
 from contacts.models import Person
@@ -12,4 +12,19 @@ class PersonCreation(CreateAPIView):
     serializer_class = PersonSerializer
     
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+        response = super().create(request, *args, **kwargs)
+        return response
+
+class PersonDestroy(DestroyAPIView):
+    queryset = Person.objects.all()
+    lookup_field = 'id'
+    
+    def delete(request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        return response
+
+class PersonRetrieveUpdate(RetrieveUpdateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    lookup_field = 'id'
+
